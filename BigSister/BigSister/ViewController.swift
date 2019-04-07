e1f2197b44a171befa5dbcc115fdc6be191dd655
yewwoei <yewwoei.low@outlook.com>
2019-04-06 20:42:58 -0700

    start ios app

---
+++ BigSister/BigSister/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BigSister
+//
+//  Created by Aiden Low Yew Woei on 4/6/19.
+//  Copyright © 2019 JAJ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
