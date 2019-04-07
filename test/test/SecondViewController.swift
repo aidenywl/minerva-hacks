812b33771d64c7d4ad2887cb031e085d40632b49
yewwoei <yewwoei.low@outlook.com>
2019-04-07 08:16:48 -0700

    add location sending

--- test/test/SecondViewController.swift
+++ test/test/SecondViewController.swift
@@ -13,25 +13,57 @@ import CoreLocation
 class SecondViewController: UIViewController, CLLocationManagerDelegate{
     
     @IBOutlet weak var idLabel: UILabel!
-    var timer: Timer!
     var locationManager: CLLocationManager!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        locationTrackerWithTimer()
         locationManager = CLLocationManager()
         locationManager.requestAlwaysAuthorization()
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+        }
     }
     
-    func locationTrackerWithTimer() {
-        // Scheduling timer to Call the function "updateCounting" with the interval of 1 seconds
-        timer = Timer.scheduledTimer(timeInterval: 5, target: self, selector: #selector(sendLocation), userInfo: nil, repeats: true)
+    override func viewDidAppear(_ animated: Bool) {
+        sleep(1)
+        idLabel.text="ID: \(ID.value)"
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let locValue: CLLocationCoordinate2D = manager.location?.coordinate else { return }
+        print("locations = \(locValue.latitude) \(locValue.longitude)")
+        let json: [String: Any] = [
+            "client_id": ID.value,
+            "longitude": locValue.longitude,
+            "latitude": locValue.latitude,
+            "unix_time": Int64(Date().timeIntervalSince1970*1000)
+        ]
         
-    }
-    
-    @objc func sendLocation() {
-        print(Int64(Date().timeIntervalSince1970*1000))
+        let jsonData = try? JSONSerialization.data(withJSONObject: json)
+        
+        // create post request
+        let url = URL(string: "https://33132f65.ngrok.io/save_location_and_time")!
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        
+        // insert json data to the request
+        request.httpBody = jsonData
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {
+                print(error?.localizedDescription ?? "No data")
+                return
+            }
+            let responseJSON = try? JSONSerialization.jsonObject(with: data, options: [])
+            if let responseJSON = responseJSON as? [String: Any] {
+                print(responseJSON)
+            }
+        }
+        
+        task.resume()
     }
     
     
--- test/test/TestViewController.swift
+++ test/test/TestViewController.swift
@@ -7,7 +7,10 @@
 //
 
 import UIKit
-var ID: String = "-1"
+
+struct ID {
+    static var value = "-1"
+}
 
 class TestViewController: UIViewController {
     
@@ -18,13 +21,13 @@ class TestViewController: UIViewController {
     
     func setID(id: String) {
         // set ID
-        ID = id
-        print(ID)
+        ID.value = id
+        print(ID.value)
         
     }
     
     @IBAction func pressSomeButton(_ sender: UIButton) {
-        let idURL = URL(string: "https://53d48747.ngrok.io/get_id")!
+        let idURL = URL(string: "https://33132f65.ngrok.io/get_id")!
         let task = URLSession.shared.dataTask(with: idURL){(data, response, error) in
             if (error == nil) {
                 let loadedID = String(bytes: data!, encoding: .utf8)!
