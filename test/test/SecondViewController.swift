e5a5f7587a37c4cd364e13fd007aa86075b12ff3
yewwoei <yewwoei.low@outlook.com>
2019-04-07 05:07:48 -0700

    got timer working

--- test/test/SecondViewController.swift
+++ test/test/SecondViewController.swift
@@ -10,9 +10,22 @@ import UIKit
 
 class SecondViewController: UIViewController {
     
+    var timer: Timer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        locationTrackerWithTimer()
+    }
+    
+    func locationTrackerWithTimer() {
+        // Scheduling timer to Call the function "updateCounting" with the interval of 1 seconds
+        timer = Timer.scheduledTimer(timeInterval: 5, target: self, selector: #selector(sendLocation), userInfo: nil, repeats: true)
+        
+    }
+    
+    @objc func sendLocation() {
+        print(Int64(Date().timeIntervalSince1970*1000))
     }
     
     
