fa1318a8c85bd5cd3f906e9ac4a25f9d56df37e3
yewwoei <yewwoei.low@outlook.com>
2019-04-07 04:32:24 -0700

    add request for permission and second screen

---
+++ test/test/ID.swift
@@ -0,0 +1,21 @@
+//
+//  Id.swift
+//  test
+//
+//  Created by Aiden Low Yew Woei on 4/7/19.
+//  Copyright © 2019 Jaivignesh Venugopal. All rights reserved.
+//
+
+import UIKit
+
+class ID {
+    let value: String
+    
+    init(id: String) {
+        self.value = id
+    }
+    
+    func getID() -> String {
+        return value
+    }
+}
