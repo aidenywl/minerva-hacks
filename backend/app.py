d7f7780a7fb6c94bb76b39cef4bebb61bfa736ea
Jaivignesh Venugopal <smugs1995@gmail.com>
2019-04-07 02:08:25 -0700

    insert data into database

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,22 @@
+import mysql.connector
 from flask import Flask, request
+import random
 
 app = Flask(__name__)
+
+
+my_db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="",
+    database="minerva"
+)
+
+my_cursor = my_db.cursor()
+
+
+def test():
+    print('hey there')
 
 
 @app.route('/')
@@ -8,14 +24,24 @@ def index():
     return 'index'
 
 
+@app.route('/get_id', methods=['GET'])
+def generate_id():
+    return str(random.randint(100000000, 999999999))
+
+
 @app.route('/location_time', methods=['POST'])
 def save_location_and_time():
     if request.method == 'POST':
-        id = request.args.get('id')
-        location = request.args.get('location')
-        time = request.args.get('time')
+        client_id = request.args.get('id')
+        latitude = request.args.get('latitude')
+        longitude = request.args.get('longitude')
+        unix_time = request.args.get('time')
+
+        add_data_command = "INSERT INTO geodata (client_id, latitude, longitude, unix_time) VALUES (%s, %s, %s, %s)"
+        add_data_command_variables = (client_id, latitude, longitude, unix_time)
+        my_cursor.execute(add_data_command, add_data_command_variables)
+        my_db.commit()
 
 
 if __name__ == '__main__':
     app.run()
-
