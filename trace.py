212cebc0b4cf0943b75095accfd4d95cde2a187c
Jaivignesh Venugopal <smugs1995@gmail.com>
2019-04-07 04:16:42 -0700

    implement google map plotter

---
+++ trace.py
@@ -0,0 +1,48 @@
+import sys
+import mysql.connector
+import gmplot
+import os
+from IPython import embed
+
+
+my_db = None
+my_cursor = None
+client_id = None
+html_file = 'map.html'
+api_key = 'AIzaSyAer_9aqaqbuEWcdKsjwssdDAd7E71iJlM'
+
+
+def main():
+    my_db = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        passwd="",
+        database="minerva"
+    )
+
+    my_cursor = my_db.cursor()
+    command = """SELECT * FROM intercepts WHERE client_id='%s' ORDER BY unix_time DESC""" % (client_id,)
+    my_cursor.execute(command)
+    intercepts = my_cursor.fetchall()
+
+    latitude_list = []
+    longitude_list = []
+
+    # sum1 = 0;
+    # sum2 = 0;
+    for intercept in intercepts:
+        latitude_list.append(intercept[3])
+        longitude_list.append(intercept[4])
+
+    google_map = gmplot.GoogleMapPlotter(30.3164945, 78.03219179999999, 13)
+    google_map.apikey = api_key
+
+    google_map.scatter([30.3358376, 30.307977, 30.3216419], [77.8701919, 78.048457, 78.0413095], '#FF0000', size=100, marker=True)
+
+    google_map.draw(html_file)
+    os.system("open " + html_file)
+
+
+if __name__ == '__main__':
+    client_id = sys.argv[1]
+    main()
