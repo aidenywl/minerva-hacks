68ab818b71533d5696ddbe3c55c2f45c51526643
Jaivignesh Venugopal <smugs1995@gmail.com>
2019-04-07 02:30:06 -0700

    add intercept records

--- backend/app.py
+++ backend/app.py
@@ -15,32 +15,47 @@ my_db = mysql.connector.connect(
 my_cursor = my_db.cursor()
 
 
-def test():
-    print('hey there')
-
-
 @app.route('/')
 def index():
     return 'index'
 
 
 @app.route('/get_id', methods=['GET'])
-def generate_id():
+def get_id():
     return str(random.randint(100000000, 999999999))
 
 
-@app.route('/location_time', methods=['POST'])
+@app.route('/save_location_and_time', methods=['POST'])
 def save_location_and_time():
     if request.method == 'POST':
-        client_id = request.args.get('id')
+        client_id = request.args.get('client_id')
         latitude = request.args.get('latitude')
         longitude = request.args.get('longitude')
-        unix_time = request.args.get('time')
+        unix_time = request.args.get('unix_time')
 
-        add_data_command = "INSERT INTO geodata (client_id, latitude, longitude, unix_time) VALUES (%s, %s, %s, %s)"
-        add_data_command_variables = (client_id, latitude, longitude, unix_time)
-        my_cursor.execute(add_data_command, add_data_command_variables)
+        add_geodata_command = "INSERT INTO geodata (client_id, latitude, longitude, unix_time) VALUES (%s, %s, %s, %s)"
+        add_geodata_command_variables = (client_id, latitude, longitude, unix_time)
+        my_cursor.execute(add_geodata_command, add_geodata_command_variables)
         my_db.commit()
+    else:
+        print('Not a POST request')
+
+
+@app.route('/add_interceptor', methods=['POST'])
+def add_interceptor():
+    if request.method == 'POST':
+        client_id = request.args.get('client_id')
+        interceptor_id = request.args.get('interceptor_id')
+        latitude = request.args.get('latitude')
+        longitude = request.args.get('longitude')
+        unix_time = request.args.get('unix_time')
+
+        add_interceptor_command = "INSERT INTO intercepts (client_id, interceptor_id, latitude, longitude, unix_time) VALUES (%s, %s, %s, %s, %s)"
+        add_interceptor_command_variables = (client_id, interceptor_id, latitude, longitude, unix_time)
+        my_cursor.execute(add_interceptor_command, add_interceptor_command_variables)
+        my_db.commit()
+    else:
+        print('Not a POST request')
 
 
 if __name__ == '__main__':
